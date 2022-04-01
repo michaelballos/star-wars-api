7843d632339d6dd7f9b4c8ef1418afb3b185d0ca
michaelballos <mdb3091207@gmail.com>
2022-04-01 17:41:50 -0500

    Add Planet API
    Fix getStaticProps Typo

--- pages/planets/index.tsx
+++ pages/planets/index.tsx
@@ -3,21 +3,27 @@ import Link from 'next/link';
 import { GetStaticProps, NextPage } from 'next';
 import { GetPlanetResults, Planet } from '../../types';
 
-
 const Planets: NextPage<{ planets: Planet[] }> = ({ planets }) => {
+
+/*
   const planetList = planets.map(planet => (
     <div key={planet.name}>
       <Link href={`/planets/${planet.name}`}>
         <h1>
-          <a>{planet.name}</a>
+          <a>
+            {planet.name}
+          </a>
         </h1>
       </Link>
     </div>
   ));
+*/
 
   return (
     <div className="planetsPage">
-      {planetList}
+
+      {JSON.stringify(planets)}
+
     <Link href='/'>
       <a>
         Go Back Home &rarr;
@@ -28,15 +34,16 @@ const Planets: NextPage<{ planets: Planet[] }> = ({ planets }) => {
 }
 
 
-export const gestaticProps: GetStaticProps = async (context) => {
+export const getStaticProps: GetStaticProps = async (context) => {
   const res = await fetch('https://swapi.dev/api/planets/');
   const { results }: GetPlanetResults = await res.json();
-
+  console.log(results);
   return {
     props: {
       planets: results,
     },
   }
+  
 }
 
 export default Planets
